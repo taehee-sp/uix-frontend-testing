0d19568b646bc7a8063a6579133ce6c82079eb3a
Taehee Kim <rabolution@gmail.com>
2024-07-06 10:48:06 +0900

    feat: saas-list 필터 구현

--- src/app/domains/saas/saas_list/saas_list.cljs
+++ src/app/domains/saas/saas_list/saas_list.cljs
@@ -1,12 +1,32 @@
 (ns app.domains.saas.saas-list.saas-list 
   (:require [app.domains.saas.saas-list.saas-list-item :refer [SaasListItem]]
-            [uix.core :refer [$ defui]]))
+            [uix.core :refer [$ defui use-state]]))
 
 (defui SaasList [{:keys [saas-list]}]
-(let [result-list (->> saas-list
+(let [[selected set-selected!] (use-state "all")
+      saas-with-payment-list (filter :last-paid-at saas-list)
+      result-list (->> (if (= selected "all")
+                         saas-list
+                         saas-with-payment-list)
                        (sort-by :last-paid-at)
                        (reverse))]
-    ($ :ul {}
-     (for [saas result-list]
-       ($ SaasListItem {:key (:id saas)
-                        :saas saas})))))
+    ($ :div {}
+       ($ :fieldset {}
+          ($ :legend {} "필터")
+          ($ :label {}
+             (str "전체 " (count saas-list))
+             ($ :input {:type "radio"
+                        :value "all"
+                        :checked (= selected "all")
+                        :on-change #(set-selected! "all")}))
+          ($ :label {}
+             (str "결제 내역 있는 SaaS " (count saas-with-payment-list))
+             ($ :input {:type "radio"
+                        :value "with-payment"
+                        :checked (= selected "with-payment")
+                        :on-change #(set-selected! "with-payment")}))
+         )
+       ($ :ul {}
+          (for [saas result-list]
+            ($ SaasListItem {:key (:id saas)
+                             :saas saas}))))))
--- src/test/smply/domains/saas/saas_list/saas_list_test.cljs
+++ src/test/smply/domains/saas/saas_list/saas_list_test.cljs
@@ -1,7 +1,7 @@
 (ns test.smply.domains.saas.saas-list.saas-list-test
   (:require [app.domains.saas.saas-list.saas-list-stories :refer [saas-list-stories]]
-            [test.siheom.siheom :refer [have-text-content? query render
-                                        run-siheom]]
+            [test.siheom.siheom :refer [checked? click! have-text-content?
+                                        query render run-siheom]]
             [test.vitest.support :refer [describe it]]))
 
 
@@ -11,6 +11,8 @@
      (run-siheom
       (render (get saas-list-stories "여러 개인 경우"))
 
+      (checked? (query "radio" "전체 5"))
+
       (have-text-content?
        (query "listitem" #"")
         ["Notion2024년 6월 27일 결제"
@@ -18,5 +20,14 @@
          "GitHub2024년 6월 15일 결제"
          "Zoom"
          "Slack"] )
+      
+      (click! (query "radio" "결제 내역 있는 SaaS 3"))
+
+      (have-text-content?
+       (query "listitem" #"")
+       ["Notion2024년 6월 27일 결제"
+        "Asana2024년 6월 20일 결제"
+        "GitHub2024년 6월 15일 결제"])
+      
       ))
 )
